37b5dc54b7fd052d8cd92bf17b7ae9c5ef64a5ea
PvD <pvdzxc@gmail.com>
2023-12-07 21:45:03 +0700

    Create Queries.sql
    Template for Query, currently create Article and Edit, review the article

---
+++ Queries.sql
@@ -0,0 +1,34 @@
+SELECT * FROM author;
+SELECT * FROM article;
+SELECT * FROM publishedarticle;
+SELECT * FROM edit_log;
+SELECT * FROM review_log;
+SELECT * FROM Bill;
+SELECT * FROM review_log;
+SELECT * FROM review_log;
+SELECT * FROM review_log;
+SELECT * FROM review_log;
+
+DELETE FROM article
+WHERE ArticleID < 100;
+DELETE FROM edit_log
+WHERE ArticleID < 100;
+DELETE FROM review_log
+WHERE ArticleID < 100;
+
+CALL ProcInsertArticle('Bao luc hoc duong', 'Ban be danh nhau, sut dau chay mau', 'Education', 123);
+CALL ProcInsertArticle('Viet Nam Phong ten lua vao mat troi', 'Sieu tau vu tru Lac Long Quan sau 3 thang di vao quy dao bay tien gan mat troi, hien dang vao qua trinh tien hanh lien ket quy dao xoay quanh mat troi', 'Science', 123);
+CALL ProcInsertArticle('bai bao 3', 'so it is', 'World', 123);
+CALL ProcInsertEditLog(3,'Bạn bè đánh nhau, sứt đầu chảy máo');
+CALL ProcInsertEditLog(3,'123 Bạn bè đánh nhao, sứt đầu chảy máo');
+CALL ProcInsertReviewLog(3,'Bài Viết quá ngắn gọn', 'Edit');
+CALL ProcInsertReviewLog(3,'Sai trính tả rôif', 'Edit');
+CALL ProcInsertReviewLog(3,'Thoi m nghi luon di', 'Reject');
+CALL ProcInsertReviewLog('2','Bai viet hay, 10 diem', 'Accept');
+CALL ProcInsertReviewLog('4','Bai viet hay, 10 diem', 'Accept');
+-- Error Code: 1452. Cannot add or update a child row: a foreign key constraint
+-- fails (`newspaper_database`.`bill`, CONSTRAINT `fk_accountant_bill` 
+-- FOREIGN KEY (`BAccountantID`) REFERENCES `accountant` (`AccountantID`))
+SELECT FindAQuiteFreeAccountant() ;
+ROLLBACK;
+COMMIT;
